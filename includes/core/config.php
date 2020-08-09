e57a44102bba05b43c52e55547d898d9a787ed68
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-08-09 19:37:22 +0800

    Fix config prefex

--- includes/core/config.php
+++ includes/core/config.php
@@ -15,8 +15,9 @@
 	//Defining Global Variables
 	define('MP_PREFIX', 'mp_'); 
 
-	define('MP_ORDER_TABLE', MP_PREFIX.'orders');
+	define('MP_ORDERS_TABLE', MP_PREFIX.'orders');
 	define("MP_ORDER_TABLE_FIELD", "(stid, opid, wpid, created_by, date_created, `status`)");
+
 
 	define('MP_ORDER_ITEMS_TABLE', MP_PREFIX.'order_items');
 	define("MP_ORDER_ITEMS_TABLE_FIELD", "(odid, pdid, quantity, order_rev_id, date_created)");
