697ead84a576f4deb9f65be9aaf60e098b887139
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-17 21:00:45 +0800

    Fixed some changes in process.

--- includes/api/v1/store/class-process.php
+++ includes/api/v1/store/class-process.php
@@ -24,7 +24,7 @@
             // Get Order ID and status order (received/rejected)
             $date = MP_Globals:: date_stamp(); 
             $table_store = TP_STORES_TABLE;
-            $table_tp_revs = 'tp_revisions';
+            $table_tp_revs = TP_REVISIONS_TABLE;
             $table_ord = MP_ORDERS_TABLE;                                     
             $table_mp_revs = MP_REVISIONS_TABLE;
             $fields_mp_revs = MP_REVISIONS_TABLE_FIELD; 
@@ -51,9 +51,9 @@
             }
             
             // Step 3: Check if required parameters are passed
-            if (!isset($_POST["odid"]) 
-                || !isset($_POST["stage"])
-                || !isset($_POST["stid"])) {
+            if (!isset($odid) 
+                || !isset($stage)
+                || !isset($stid)) {
                 return array(
                         "status" => "unknown",
                         "message" => "Please contact your administrator. Request unknown!",
@@ -61,45 +61,44 @@
             }
 
             // Step 4: Check if parameters passed are empty
-            if (empty($_POST["odid"]) 
-                || empty($_POST["stage"])
-                || empty($_POST["stid"])) {
+            if (empty($odid) 
+                || empty($stid)
+                || empty($stage)) {
                 return array(
                         "status" => "failed",
                         "message" => "Required fields cannot be empty.",
                 );
             }
 
-            // Step 5: Check if stage input is not received or rejected
-            if ($stage === 'pending' || $stage === 'completed') {
+            // Step 5: Check if stage input is not received or cancelled
+            if ($stage === 'pending' || $stage === 'completed' || $stage === 'shipping') {
                 return array(
                     "status" => "failed",
                     "message" => "This process is not for $stage.",
                 );
             }
 
-            // Step 6: Validate store
-            $verify_store =$wpdb->get_row("SELECT ID FROM $table_store WHERE ID = '$stid' ");
-            $verify_store_stat =$wpdb->get_row("SELECT child_val AS status FROM $table_tp_revs WHERE ID = (SELECT status FROM $table_store WHERE ID = '$stid') ");
-            if (!$verify_store || !($verify_store_stat->status === '1')) {
-                return array(
-                    "status" => "failed",
-                    "message" => "No store found.",
-                );
-            }
+            // Step 6: Check if stage input is received or cancelled
+            if ($stage === 'received' || $stage === 'cancelled') {
 
-            // Step 7: Validate order using order id and store id
-            $verify_order =$wpdb->get_row("SELECT ID FROM $table_ord WHERE ID = '$odid' AND stid = '$stid' ");
-            if (!$verify_order) {
-                return array(
-                    "status" => "failed",
-                    "message" => "No order found.",
-                );
-            }
-
-            // Step 8: Check if stage input is received or rejected
-            if ($stage === 'received' || $stage === 'rejected' || $stage === 'shipping') {
-
+                // Step 7: Validate store and store staus if active
+                $verify_store =$wpdb->get_row("SELECT ID FROM $table_store WHERE ID = '$stid' "); // Check if store is exist or not
+                $verify_store_stat =$wpdb->get_row("SELECT child_val AS status FROM $table_tp_revs WHERE ID = (SELECT status FROM $table_store WHERE ID = '$stid') "); // If exist, check status
+                if (!$verify_store || !($verify_store_stat->status === '1')) {
+                    return array(
+                        "status" => "failed",
+                        "message" => "No store found.",
+                    );
+                }
+    
+                // Step 8: Validate order using order id and store id in mp orders table
+                $verify_order =$wpdb->get_row("SELECT ID FROM $table_ord WHERE ID = '$odid' AND stid = '$stid' ");
+                if (!$verify_order) {
+                    return array(
+                        "status" => "failed",
+                        "message" => "No order found.",
+                    );
+                }
 
                 // Step 9: Check if order status is the same in stage input
                 $verify_stage = $wpdb->get_row("SELECT child_val AS status FROM $table_mp_revs WHERE ID = (SELECT `status` FROM $table_ord WHERE ID = '$odid' AND stid = '$stid') ");
@@ -110,34 +109,27 @@
                     );
                 }
                 
-                if ($stage === 'received' || $stage === 'rejected'){
-                    // Step 10: Check if order status is pending
-                    if (!($verify_stage->status === 'pending')) {
-                        return array(
-                            "status" => "failed",
-                            "message" => "This order can't be $stage.",
-                        );
-                    }
-                }
-                
-                if ($stage === 'shipping'){
-                    // Step 11: Check if order status is received
-                    if (!($verify_stage->status === 'received')) {
-                        return array(
-                            "status" => "failed",
-                            "message" => "This order can't be $stage.",
-                        );
-                    }
+                // Step 10: Check if order status is pending
+                if (!($verify_stage->status === 'pending')) {
+                    return array(
+                        "status" => "failed",
+                        "message" => "This order can't be $stage.",
+                    );
                 }
             
-                // Step 12: Update query
+                // Step 11: Update query
                 $wpdb->query("START TRANSACTION");
                 // Insert into table revision (type = orders, order id, key = status, value = status value, customer id and date)
+                
+                if ($stage === 'cancelled') {
+                    $wpdb->query("INSERT INTO $table_mp_revs $fields_mp_revs VALUES ('orders', '$odid', 'cancel_by', 'store', '$wpid', '$date') ");
+                }
+
                 $insert = $wpdb->query("INSERT INTO $table_mp_revs $fields_mp_revs VALUES ('orders', '$odid', 'status', '$stage', '$wpid', '$date') ");
                 $order_status = $wpdb->insert_id;
                 $result = $wpdb->query("UPDATE $table_ord SET created_by = '$wpid', status = '$order_status' WHERE ID IN ($odid) ");
 
-                // Step 13: Check result
+                // Step 12: Check result
                 if ( $insert < 1 || $result < 1 ) {
                     $wpdb->query("ROLLBACK");
                     return array(
@@ -151,7 +143,6 @@
                         "message" => "Order has been $stage successfully."
                     );
                 }
-
             }
             else{
                 return array(
